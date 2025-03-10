8c661aaff4bbec90903885969e11da65b6f83f65
nanduaji <nanduaji64@gmail.com>
2025-03-10 15:44:22 +0530

    implement book management features: add update, delete, and price range query functionalities

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,6 +1,7 @@
 const Book = require('../models/booksModels');
 
 const booksController = {
+    // Get All Books
     getBooks: async (req, res) => {
         try {
             const books = await Book.find();
@@ -20,7 +21,7 @@ const booksController = {
             });
         }
     },
-
+    // Add Books
     addBooks: async (req, res) => {
         try {
             const { title, author, price, stock } = req.body;
@@ -70,8 +71,7 @@ const booksController = {
             });
         }
     },
-
-
+    // Get Books By Author
     getBooksByAuthor: async (req, res) => {
         try {
             const {author} = req.body;
@@ -100,6 +100,109 @@ const booksController = {
             });
         }
     },
+    // Update Books By Title (Price and Stock)
+    updateBooks: async (req, res) => {
+        try {
+            const {price,stock,title} = req.body;
+            if(!price){
+                return res.status(400).json({
+                    success: false,
+                    statusCode: 400,
+                    message: 'Price is required',
+                    data: null
+                });
+            }
+            if(!stock){
+                return res.status(400).json({
+                    success: false,
+                    statusCode: 400,
+                    message: 'Stock is required',
+                    data: null
+                });
+            }
+            if(!title){
+                return res.status(400).json({
+                    success: false,
+                    statusCode: 400,
+                    message: 'Title is required',
+                    data: null
+                });
+            }
+            const updatedBook = await Book.findOneAndUpdate(
+                { title: title },
+                { $set: { price: price, stock: stock } },
+                { new: true }
+            );
+            res.status(200).json({
+                success: true,
+                statusCode: 200,
+                message: 'Books retrieved successfully',
+                count: updatedBook.length,
+                data: updatedBook
+            });
+        } catch (err) {
+            res.status(500).json({
+                success: false,
+                statusCode: 500,
+                message: 'Server error',
+                error: err.message
+            });
+        }
+    },
+    // Delete Book By Title 
+    deleteBooks: async (req, res) => {
+        try {
+            const {title} = req.body;
+            if(!title){
+                return res.status(400).json({
+                    success: false,
+                    statusCode: 400,
+                    message: 'Title is required',
+                    data: null
+                });
+            }
+            const deletedbook = await Book.findOneAndDelete(
+                { title: title }
+            );
+            res.status(200).json({
+                success: true,
+                statusCode: 200,
+                message: 'Book deleted successfully',
+                count: deletedbook.length,
+                data: deletedbook
+            });
+        } catch (err) {
+            res.status(500).json({
+                success: false,
+                statusCode: 500,
+                message: 'Server error',
+                error: err.message
+            });
+        }
+    },
+    // Querying books with a price range priced between $10 and $20
+    getBooksByPrice: async (req, res) => {
+        try {
+            const books = await Book.find({
+                price: { $gte: 10, $lte: 20 }
+            });
+            res.status(200).json({
+                success: true,
+                statusCode: 200,
+                message: 'Books retrieved successfully',
+                count: books.length,
+                data: books
+            });
+        } catch (err) {
+            res.status(500).json({
+                success: false,
+                statusCode: 500,
+                message: 'Server error',
+                error: err.message
+            });
+        }
+    },
+
 };
 
 module.exports = booksController;
--- router/routes.js
+++ router/routes.js
@@ -1,8 +1,11 @@
-const { getBooks, addBooks, getBooksByAuthor } = require('../controllers/booksController');
+const { getBooks, addBooks, getBooksByAuthor, updateBooks, deleteBooks, getBooksByPrice } = require('../controllers/booksController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 const routes = require('express').Router();
 routes.get('/getBooks', authMiddleware, getBooks);
 routes.post('/getBooksByAuthor',authMiddleware,getBooksByAuthor);
 routes.post('/addBooks', authMiddleware, addBooks);
+routes.post('/updateBooks',authMiddleware,updateBooks);
+routes.post('/deleteBooks',authMiddleware,deleteBooks);
+routes.get('/getBooksByPrice',authMiddleware,getBooksByPrice);
 module.exports = routes;
