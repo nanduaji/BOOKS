fba9e5ba37cbe7add3977cd856561eefaab14739
nanduaji <nanduaji64@gmail.com>
2025-03-10 12:22:53 +0530

    change server port from 3001 to 3000

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
-const PORT = 3001;
+const PORT = 3000;
 const routes = require('./router/routes');
 
 app.use('/api', routes);
